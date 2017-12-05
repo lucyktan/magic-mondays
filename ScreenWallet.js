8d3b43eb30d4dfd3529a430492d046ae13e3de57
Your Name <your@email.address>
2017-12-04 16:03:50 -0800

    added listview

--- ScreenWallet.js
+++ ScreenWallet.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Tab, Tabs } from 'native-base';
+import { Button, Container, Header, Content, Icon, List, ListItem, Left, Body, Right, Separator, Switch, Thumbnail, Tab, Tabs } from 'native-base';
 import {
   Image,
+  ListView,
   Platform,
   StyleSheet,
   View,
@@ -10,33 +11,81 @@ import {
   TouchableOpacity
 } from 'react-native';
 
+
+const dataSource = new ListView.DataSource({
+    rowHasChanged: (r1, r2) => r1 !== r2,
+    sectionHeaderHasChanged : (s1, s2) => s1 !== s2
+});
+
+const dataBlob = {
+  "List1": [{'id': '1'}, {'id': '2'}, {'id': '3'}]
+};
+
 class ScreenWallet extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: `Welcome ${navigation.state.params.screen}`
   });
 
   render() {
-    const { params } = this.props.navigation.state;
+    const listData = dataSource.cloneWithRowsAndSections(
+        dataBlob,
+        Object.keys(dataBlob)
+    );
+
+    const { state, navigate, goBack } = this.props.navigation;
     return (
       <Container>
         <View >
             {Platform.OS === 'ios' && <View style={styles.statusBar} />}
             <View style={styles.toolbarContainer} >
-                <TouchableOpacity onPress={() => this.props.navigator.pop()} >
+                <TouchableOpacity onPress={() => goBack()} >
                     <Image style={styles.backImg} source={require('./images/back_arrow_black.png')} />
                 </TouchableOpacity>
-                <Text style={styles.titleTxt} >My Offers</Text>
+                <Text style={styles.titleText} >My Offers</Text>
             </View>
         </View>
-        <Tabs initialPage={2} tabBarUnderlineStyle={{height: 3, backgroundColor: '#CC0000'}}>
+        <Tabs initialPage={0} tabBarUnderlineStyle={{height: 2, backgroundColor: '#CC0000'}}>
           <Tab heading="RECENTLY ADDED" tabStyle={{backgroundColor:"#FFFFFF"}} 
-               textStyle={{fontSize: 14, color: 'gray'}}
-               activeTextStyle={{fontSize: 14, color: 'black'}}>
-            <Text>"asdf"</Text>
+               textStyle={{fontSize: 12, color: 'gray'}}
+               activeTextStyle={{fontSize: 12, color: 'black'}}>
+            <View style={{height: 200}}>
+            <List>
+              <ListItem>
+                <Text style={{fontWeight: 'bold'}}>NEW! 
+                  <Text style={{fontWeight: 'normal'}}> FRIENDS & FAMILY EVENT!{"\n"}
+                      <Text style={{fontWeight: 'bold', color: '#CC0000'}}>EXTRA 30%, 25%, 15% OR 10% OFF!{"\n"}
+                        <Text style={{fontWeight: 'normal', fontSize: 10, color: 'black'}}>valid 11/30/2017 - 12/12/2017</Text>
+                      </Text>
+                  </Text>
+                </Text>
+              </ListItem>
+            </List>
+            <ListView  style={{height: 100}}
+              dataSource={listData}
+
+                renderRow={(record) => 
+                        <View>
+                          <ListItem onPress={() => navigate("ScreenPrize", { screen: "Screen Prize" })}>
+                            <View>
+                              <Text>{this.props.navigation.state.params.user}{record.id}</Text>
+                            </View>
+                          </ListItem>
+                        </View>
+                }
+                renderLeftHiddenRow={item => null}
+                renderRightHiddenRow={(item) => null}
+            />
+            </View>
+            <View>
+            <TouchableHighlight style={{height: 30, width: 125, marginLeft: 125, marginTop: 15, backgroundColor: 'gray', justifyContent: 'center', alignItems: 'center'}}
+              onPress={() => navigate("ScreenPrize", { screen: "Screen Prize" })}>
+              <Text style={{fontWeight: 'normal', fontSize: 12, color: 'white'}}>ADD AN OFFER</Text>
+            </TouchableHighlight>
+            </View>
           </Tab>
           <Tab heading="EXPIRING SOON"  tabStyle={{backgroundColor:"#FFFFFF"}}
-               textStyle={{fontSize: 14, color: 'gray'}}
-               activeTextStyle={{fontSize: 14, color: 'black'}}>
+               textStyle={{fontSize: 12, color: 'gray'}}
+               activeTextStyle={{fontSize: 12, color: 'black'}}>
             <Text>"asdf"</Text>
           </Tab>
         </Tabs>
@@ -52,7 +101,9 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   titleText: {
-    fontSize: 22
+    fontSize: 18,
+    fontWeight: '500',
+    marginLeft: 20
   },
   button: {
     alignSelf: 'stretch',
